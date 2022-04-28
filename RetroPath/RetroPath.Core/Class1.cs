b4bf90f9844f64459bc6d55a1c700190b0b93133
TraceLD <lukaszdabrowski881@gmail.com>
2022-04-28 10:23:02 +0100

    scaffold C# projects

---
+++ RetroPath/RetroPath.Core/Class1.cs
@@ -0,0 +1,14 @@
+﻿using GraphMolWrap;
+
+namespace RetroPath.Core;
+
+public static class Class1
+{
+    public static string Test()
+    {
+        using var mol = RWMol.MolFromSmiles("C(COC(=O)O)C(=O)O");
+        var str = mol.MolToSmiles();
+
+        return str;
+    }
+}
