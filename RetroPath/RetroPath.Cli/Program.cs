1300f9f87d46ca9ddd389529d375debb81c84a5e
TraceLD <lukaszdabrowski881@gmail.com>
2022-05-02 17:58:01 +0100

    update main program

--- RetroPath/RetroPath.Cli/Program.cs
+++ RetroPath/RetroPath.Cli/Program.cs
@@ -7,8 +7,10 @@ using GraphMolWrap;
 
 var inputConfig = new InputConfiguration(
     @"C:\Users\Lukasz\Documents\projects\retropath-standalone\data\retrorules_rr02_rp2_flat_all.csv",
-    "sink_file",
-    "source_fule",
+    @"C:\Users\Lukasz\Documents\projects\rp\RetroPath2.0\tutorial_data\pinocembrin\sink_B.csv",
+    @"C:\Users\Lukasz\Documents\projects\rp\RetroPath2.0\tutorial_data\pinocembrin\source.csv",
+    1000,
+    "cofacts_file",
     1000,
     0,
     1000,
@@ -18,6 +20,8 @@ var inputConfig = new InputConfiguration(
 // var rulesParser = new RulesParser(inputConfig);
 //var parsedRules = rulesParser.Parse(inputConfig.RulesFilePath);
 
-using var mol = RWMol.MolFromSmiles("fdsafdsafdsafdsa");
+using var compoundParser = new CompoundParser(inputConfig);
+var parsedSinks = compoundParser.Parse(inputConfig.SinkFilePath, ChemicalType.Sink);
+var parsedSources = compoundParser.Parse(inputConfig.SourceFilePath, ChemicalType.Source);
 
 Console.WriteLine("Goodbye!");
