15fd3f0aac6d20afcea5b3dfb5cab78863692628
TraceLD <lukaszdabrowski881@gmail.com>
2022-05-02 18:04:37 +0100

    add an abstraction over RDKit C++ Inchi functions

---
+++ RetroPath/RetroPath.RDKit.Abstractions/Inchi.cs
@@ -0,0 +1,28 @@
+﻿using GraphMolWrap;
+
+namespace RetroPath.RDKit.Abstractions;
+
+public static class Inchi
+{
+    public static RWMol? InchiToMolSimple(string inchi, bool sanitise, bool removeHs)
+    {
+        RWMol? mol;
+        using (var extra = new ExtraInchiReturnValues())
+        {
+            mol = RDKFuncs.InchiToMol(inchi, extra, sanitise, removeHs);
+        }
+
+        return mol;
+    }
+
+    public static string? MolToInchiSimple(ROMol mol)
+    {
+        string? inchi;
+        using (var extra = new ExtraInchiReturnValues())
+        {
+            inchi = RDKFuncs.MolToInchi(mol, extra);
+        }
+
+        return inchi;
+    }
+}
