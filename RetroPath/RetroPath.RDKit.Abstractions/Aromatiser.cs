03e71e94f4694f01d047c8f239a8f6e05a12620e
TraceLD <lukaszdabrowski881@gmail.com>
2022-05-02 18:05:43 +0100

    add an abstraction for aromatising compounds

---
+++ RetroPath/RetroPath.RDKit.Abstractions/Aromatiser.cs
@@ -0,0 +1,25 @@
+﻿using GraphMolWrap;
+
+namespace RetroPath.RDKit.Abstractions;
+
+public class Aromatiser
+{
+    public RWMol? Aromatise(RWMol mol, bool rescueKek)
+    {
+        var temp = new RWMol(mol);
+        
+        if (RDKFuncs.setAromaticity(temp) != 0)
+        {
+            RDKFuncs.adjustHs(temp);
+            return temp;
+        }
+
+        if (!rescueKek)
+        {
+            return null;
+        }
+        
+        mol.Kekulize();
+        return mol;
+    }
+}
