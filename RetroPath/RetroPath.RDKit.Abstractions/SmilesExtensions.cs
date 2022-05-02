51fa54aaccb9d5e228a44bd5ce5fb71cc5e29e42
TraceLD <lukaszdabrowski881@gmail.com>
2022-05-02 18:01:48 +0100

    add utility function for checking SMILES if monomolecular

---
+++ RetroPath/RetroPath.RDKit.Abstractions/SmilesExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace RetroPath.RDKit.Abstractions;
+
+/// <summary>
+/// Useful extensions for working with SMILES.
+/// </summary>
+public static class SmilesExtensions
+{
+    /// <summary>
+    /// Checks whether a substrate is mono molecular.
+    /// </summary>
+    /// <param name="smiles">Substrate in the SMILES form.</param>
+    /// <returns>Returns whether a substrate is mono molecular.</returns>
+    public static bool IsMonomolecular(string smiles) => smiles.Split('.').Length == 1;
+}
