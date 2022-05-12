c255596bd5c9b4f7cda86b3c4797a96799735499
TraceLD <lukaszdabrowski881@gmail.com>
2022-05-12 13:12:38 +0200

    move ParsedGeneratedCompound record to its own file

---
+++ RetroPath/RetroPath.Core/Models/ParsedGeneratedCompound.cs
@@ -0,0 +1,15 @@
+﻿using GraphMolWrap;
+
+namespace RetroPath.Core.Models;
+
+public record ParsedGeneratedCompound(
+    List<string> TransformationIds,
+    string Inchi,
+    string? LiteInchi,
+    string Smiles,
+    RWMol? Mol,
+    List<GeneratedProductsParser.TransformationIdCoeff> TransformationIdCoeff
+) : IDisposable
+{
+    public void Dispose() => Mol?.Dispose();
+}
