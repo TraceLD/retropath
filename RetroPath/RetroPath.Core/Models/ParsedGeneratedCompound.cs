c96fd779bbfb39163e5d063c431185715c8494cc
TraceLD <lukaszdabrowski881@gmail.com>
2022-05-17 04:26:46 +0200

    add ParsedGeneratedCompound to ChemicalCompound converter

--- RetroPath/RetroPath.Core/Models/ParsedGeneratedCompound.cs
+++ RetroPath/RetroPath.Core/Models/ParsedGeneratedCompound.cs
@@ -12,4 +12,22 @@ public record ParsedGeneratedCompound(
 ) : IDisposable
 {
     public void Dispose() => Mol?.Dispose();
+
+    public ChemicalCompound ToChemicalCompound(HashSet<string>? names = null, bool disposeMol = false)
+    {
+        var mol = Mol;
+        if (disposeMol)
+        {
+            Dispose();
+            mol = null;
+        }
+
+        if (LiteInchi is null)
+        {
+            throw new NullReferenceException(nameof(LiteInchi) + " cannot be null when converting to " +
+                                             nameof(ChemicalCompound) + ".");
+        }
+
+        return new(names ?? new(), LiteInchi, Smiles, mol, false);
+    }
 }
