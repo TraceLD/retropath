86e015b70a7e5f76f73ff808d51260bc432fd318
TraceLD <lukaszdabrowski881@gmail.com>
2022-06-07 14:33:18 +0200

    port pathway loop into a class

---
+++ RetroPath/RetroPath.Core/IRetroPathLoop.cs
@@ -0,0 +1,10 @@
+﻿namespace RetroPath.Core;
+
+public interface IRetroPathLoop<TResult>
+{
+    public List<TResult> Results { get; }
+    public int I { get; }
+    
+    public void Run();
+    public void RunIteration();
+}
---
+++ RetroPath/RetroPath.Core/PathwayLoop.cs
@@ -0,0 +1,95 @@
+﻿using RetroPath.Core.Models;
+using RetroPath.Core.Models.Configuration;
+using Serilog;
+
+namespace RetroPath.Core;
+
+public class PathwayLoop : IRetroPathLoop<GlobalResult>
+{
+    private readonly InputConfiguration _inputConfiguration;
+    private readonly int _pathwayLength;
+    private readonly int _iOuter;
+    private readonly List<IGrouping<int, ReactionRule>> _rules;
+
+    private List<ChemicalCompound> _iSourcesInSink;
+    private List<ChemicalCompound> _iSourcesNotInSink;
+    private Dictionary<string, ChemicalCompound> _iSourcesAndSinks;
+
+    public List<GlobalResult> Results { get; }
+    public int I { get; private set; }
+    
+    public PathwayLoop(
+        InputConfiguration inputConfiguration,
+        int iOuter,
+        List<IGrouping<int, ReactionRule>> rules,
+        List<ChemicalCompound> starterSourcesInSink,
+        List<ChemicalCompound> starterSourcesNotInSink,
+        Dictionary<string, ChemicalCompound> starterSourcesAndSinks
+    )
+    {
+        _inputConfiguration = inputConfiguration;
+        _pathwayLength = inputConfiguration.PathwayLength;
+        _iOuter = iOuter;
+        _rules = rules;
+
+        _iSourcesInSink = starterSourcesInSink;
+        _iSourcesNotInSink = starterSourcesNotInSink;
+        _iSourcesAndSinks = starterSourcesAndSinks;
+        
+        Results = new();
+        I = 0;
+    }
+
+    public void Run()
+    {
+        while (true)
+        {
+            if (I > _pathwayLength || _iSourcesAndSinks.Count < 1 || _iSourcesNotInSink.Count < 1)
+            {
+                break;
+            }
+            
+            Log.Information("Iteration: {IInner} for source {ISource}, Number of sources to iterate through: {SourcesCount}", I, _iOuter, _iSourcesNotInSink.Count);
+            
+            RunIteration();
+        }
+    }
+
+    public void RunIteration()
+    {
+        var iRulesFirer = new RulesFirer(_iSourcesNotInSink, new(), _rules);
+        var iRes = iRulesFirer.FireRules();
+
+        using var iGpParser = new GeneratedProductsParser(iRes, _iOuter, I);
+        var iParsedProducts = iGpParser.Parse();
+
+        var iUpdater = new SourceSinkUpdater(_iSourcesAndSinks, iParsedProducts.Right, iParsedProducts.TransformationInfos, _inputConfiguration);
+        var (iNewSourcesInSink, iNewSink, iNewSources) = iUpdater.Update();
+
+        // TODO: this needs extensive testing;
+        // dispose sources that will no longer be needed;
+        if (I != 0)
+        {
+            foreach (var s in _iSourcesNotInSink)
+            {
+                s.Dispose();
+            }
+        }
+        else
+        {
+            _iSourcesNotInSink[_iOuter].Dispose();
+        }
+            
+        var iBuilder = new GlobalResultsResultsBuilder(iParsedProducts.Left, iParsedProducts.Right, iParsedProducts.TransformationInfos, _iSourcesInSink);
+        var iResults = iBuilder.Build().ToList();
+
+        Log.Information("Generated {ResCount} results", iResults.Count);
+
+        Results.AddRange(iResults);
+
+        I++;
+        _iSourcesInSink = iNewSourcesInSink;
+        _iSourcesNotInSink = iNewSources;
+        _iSourcesAndSinks = iNewSink;
+    }
+}
