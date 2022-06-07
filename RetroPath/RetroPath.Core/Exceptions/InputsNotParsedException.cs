4d54a5c8850eb06a618e008438c04af4fa449d61
TraceLD <lukaszdabrowski881@gmail.com>
2022-06-07 14:32:58 +0200

    add an exception for when inputs have not been parsed

---
+++ RetroPath/RetroPath.Core/Exceptions/InputsNotParsedException.cs
@@ -0,0 +1,16 @@
+﻿namespace RetroPath.Core.Exceptions;
+
+public class InputsNotParsedException : Exception
+{
+    public InputsNotParsedException()
+    {
+    }
+
+    public InputsNotParsedException(string message) : base(message)
+    {
+    }
+
+    public InputsNotParsedException(string message, Exception inner) : base(message, inner)
+    {
+    }
+}
