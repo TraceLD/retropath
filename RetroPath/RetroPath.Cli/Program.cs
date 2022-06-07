91f8e1da8a01adf9b279c074c550772ef75b730a
TraceLD <lukaszdabrowski881@gmail.com>
2022-06-07 14:34:33 +0200

    finish CLI

--- RetroPath/RetroPath.Cli/Program.cs
+++ RetroPath/RetroPath.Cli/Program.cs
@@ -1,112 +1,14 @@
-﻿using GraphMolWrap;
-using RetroPath.Core;
-using RetroPath.Core.Models;
-using RetroPath.Core.Models.Configuration;
-using RetroPath.Core.Parsers;
+﻿using CliFx;
 using Serilog;
 
-var inputConfig = new InputConfiguration(
-    @"C:\Users\Lukasz\Documents\projects\retropath-standalone\data\retrorules_rr02_rp2_flat_all.csv",
-    @"C:\Users\Lukasz\Documents\projects\rp\RetroPath2.0\tutorial_data\pinocembrin\sink_B.csv",
-    @"C:\Users\Lukasz\Documents\projects\rp\RetroPath2.0\tutorial_data\pinocembrin\source.csv",
-    1000,
-    "cofacts_file",
-    1000,
-    0,
-    1000,
-    4,
-    100
-);
-
+// setup the logger;
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Information()
     .WriteTo.Console()
     .CreateLogger();
 
-Log.Information("Parsing rules...");
-
-var rulesParser = new RulesParser(inputConfig);
-var parsedRules = rulesParser.Parse(inputConfig.RulesFilePath);
-
-Log.Information("Parsing sinks/cofactors/sources...");
-
-using var compoundParser = new CompoundParser(inputConfig);
-var parsedSinks = compoundParser.Parse(inputConfig.SinkFilePath, ChemicalType.Sink);
-var parsedSources = compoundParser.Parse(inputConfig.SourceFilePath, ChemicalType.Source);
-
-var (sourcesInSink, sourcesNotInSink, sourcesAndSinks) = SinkAndSourceDivider.Divide(parsedSources, parsedSinks);
-
-Log.Information("Rules: {RulesCount}", parsedRules.Count);
-Log.Information("Sinks + sources: {SpsCount}", sourcesAndSinks.Count);
-Log.Information("Sources in sink: {SisCount}", sourcesInSink.Count);
-Log.Information("Sources NOT in sink: {SnisCount}", sourcesNotInSink.Count);
-
-// TODO: extract this into its own method;
-var groupedRules = parsedRules
-    .AsParallel()
-    .GroupBy(r => r.Diameter)
-    .OrderByDescending(r => r.Key)
-    .ToList();
-
-Log.Information("Firing rules...");
-
-var rulesFirer = new RulesFirer(sourcesNotInSink, new(), groupedRules);
-var res = rulesFirer.FireRules();
-
-Log.Information("Done firing rules...");
-
-Log.Information("Parsing rules and updating sink and source...");
-
-using var gpParser = new GeneratedProductsParser(res, 0, 0);
-var parsedResults = gpParser.Parse();
-
-var updater = new SourceSinkUpdater(sourcesAndSinks, parsedResults.Right, parsedResults.TransformationInfos, inputConfig);
-var (sourceInSink, newSink, newSources) = updater.Update();
-
-Log.Information("Parsed rules and updated sink and source");
-
-var builder = new GlobalResultsResultsBuilder(parsedResults.Left, parsedResults.Right, parsedResults.TransformationInfos,
-    sourceInSink);
-var results = builder.Build();
-
-Log.Information("Generated {GlobalResultsCount} global results", results.Count());
-
-Log.Information("Goodbye");
-
-// TODO: make the recursive loop a nice class;
-// TODO: currently not using the loop for debug purposes (easier to test stuff without a recursive loop)
-List<string> Recurse(int pathwayLength, int iOuter, int iInner, InputConfiguration inputConfig, List<ChemicalCompound> iSourcesInSink, List<ChemicalCompound> iSourcesNotInSink, Dictionary<string, ChemicalCompound> iSourcesAndSinks,List<IGrouping<int, ReactionRule>> rules, List<string> results)
-{
-    if (iInner > pathwayLength || iSourcesAndSinks.Count < 1 || iSourcesNotInSink.Count < 1)
-    {
-        return results;
-    }
-    
-    var iRulesFirer = new RulesFirer(iSourcesNotInSink, new(), rules);
-    var iRes = iRulesFirer.FireRules();
-
-    using var iGpParser = new GeneratedProductsParser(iRes, iOuter, iInner);
-    var iParsedProducts = iGpParser.Parse();
-
-    var iUpdater = new SourceSinkUpdater(iSourcesAndSinks, iParsedProducts.Right, iParsedProducts.TransformationInfos,
-        inputConfig);
-    var (iNewSourcesInSink, iNewSink, iNewSources) = iUpdater.Update();
-
-    // TODO: this needs extensive testing;
-    // dispose sources that will no longer be needed;
-    if (iInner != 0)
-    {
-        foreach (var s in iSourcesNotInSink)
-        {
-            s.Dispose();
-        }
-    }
-    else
-    {
-        iSourcesNotInSink[iOuter].Dispose();
-    }
-
-    var iNewInner = iInner + 1;
-
-    return Recurse(pathwayLength, iOuter, iNewInner, inputConfig, iNewSourcesInSink, iNewSources, iNewSink, rules, results);
-}
+// setup CLI;
+return await new CliApplicationBuilder()
+    .AddCommandsFromThisAssembly()
+    .Build()
+    .RunAsync();
---
+++ RetroPath/RetroPath.Cli/RetroPathCommand.cs
@@ -0,0 +1,73 @@
+﻿using CliFx;
+using CliFx.Attributes;
+using CliFx.Infrastructure;
+using RetroPath.Core.Models.Configuration;
+using Serilog;
+
+namespace RetroPath.Cli;
+
+[Command]
+public class RetroPathCommand : ICommand
+{
+    #region Parameters
+
+    [CommandParameter(0, Name = "rules", Description = "Path to the CSV file defining reactions rules.")]
+    public string RulesFilePath { get; init; } = null!;
+
+    [CommandParameter(1, Name = "source", Description = "Path to the CSV file defining source compound(s).")]
+    public string SourceFilePath { get; init; } = null!;
+
+    [CommandParameter(2, Name = "sink", Description = "Path to the CSV file defining sink compound(s).")]
+    public string SinkFilePath { get; init; } = null!;
+    
+    [CommandParameter(3, Name = "pathway-length", Description = "Number of iterations to use. Pathway length is an important parameter regarding combanory explosion. This value will impact execution time and memory consumption. We recommend to use pathway lengths lower than or equal to 3 on standard workstations.")]
+    public int PathwayLength { get; init; }
+    
+    #endregion
+
+    #region Options
+
+    [CommandOption("cofacs", Description = "Path to the file defining cofactors. No cofactors are imported if this option is not set. Cofactors are only used for bi-substrate rules.")]
+    public string? CofactorsFilePath { get; init; }
+    
+    [CommandOption("source-mw", Description = "Maximum molecular weight in daltons (Da) above which a source compound will be filtered out.")]
+    public int SourceMw { get; init; } = 1000;
+    
+    [CommandOption("cofact-mw", Description = "Maximum molecular weight in daltons (Da) above which a cofactor compound will be filtered out.")]
+    public int CofactorMw { get; init; } = 1000;
+
+    [CommandOption("min-diameter", Description = "The minimum diameter allowed for rules.")]
+    public int MinDiameter { get; init; } = 0;
+    
+    [CommandOption("max-diameter", Description = "The minimum diameter allowed for rules.")]
+    public int MaxDiameter { get; init; } = 1000;
+
+    [CommandOption("max-structures", Description = "Number of best structures to keep as substrates for a further iteration. This value is used at the end of a RetroSMARTS iteration -- in combination with the rule score -- in order to select best products that will be used for a further iteration.")]
+    public int MaxStructures { get; init; } = 100;
+
+    [CommandOption("output-dir", Description = "Path to the directory to which the results will be written. NOTE: Any previous results in this directory will be overriden. Defaults to \"./results\"")]
+    public string OutputDir { get; init; } = Path.Combine(Directory.GetCurrentDirectory(), "results");
+
+    #endregion
+    
+    public async ValueTask ExecuteAsync(IConsole console)
+    {
+        var inputConfig = GetInputConfigurationObject();
+        var outputConfig = GetOutputConfigurationObject();
+
+        Log.Information("Writing results to: \"{ResultsDirPath}\"", outputConfig.OutputDir);
+        
+        using var rp = new RetroPath.Core.RetroPath(inputConfig, outputConfig);
+
+        await rp.ParseInputs();
+        var results = rp.Compute();
+        
+        Log.Information("Generated {GlobalResultsCount} global results", results.Count);
+    }
+
+    private InputConfiguration GetInputConfigurationObject()
+        => new(RulesFilePath, SinkFilePath, SourceFilePath, SourceMw, CofactorsFilePath, CofactorMw, MinDiameter,
+            MaxDiameter, PathwayLength, MaxStructures);
+
+    private OutputConfiguration GetOutputConfigurationObject() => new(OutputDir);
+}
