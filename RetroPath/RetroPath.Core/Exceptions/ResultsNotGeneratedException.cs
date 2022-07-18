948620e77942337db1ebabed3ab1bfac9b1d24b4
TraceLD <lukaszdabrowski881@gmail.com>
2022-07-18 11:31:06 +0100

    add ResultsNotGeneratedException

---
+++ RetroPath/RetroPath.Core/Exceptions/ResultsNotGeneratedException.cs
@@ -0,0 +1,16 @@
+﻿namespace RetroPath.Core.Exceptions;
+
+public class ResultsNotGeneratedException : Exception
+{
+    public ResultsNotGeneratedException()
+    {
+    }
+
+    public ResultsNotGeneratedException(string message) : base(message)
+    {
+    }
+
+    public ResultsNotGeneratedException(string message, Exception inner) : base(message, inner)
+    {
+    }
+}
