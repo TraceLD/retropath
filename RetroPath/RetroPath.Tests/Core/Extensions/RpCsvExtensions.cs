de229ec171102764873a1c977d553edda5cc1e7f
TraceLD <lukaszdabrowski881@gmail.com>
2022-07-18 12:02:56 +0100

    add extension method for CSV serialisation of lists of strings

---
+++ RetroPath/RetroPath.Tests/Core/Extensions/RpCsvExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using RetroPath.Core.Extensions;
+using Xunit;
+
+namespace RetroPath.Tests.Core.Extensions;
+
+public class RpCsvExtensions
+{
+    public class EnumerableStringsToCsvStringTests
+    {
+        [Fact]
+        public void ShouldReturnBracketsForEmpty()
+        {
+            var expected = "[]";
+
+            var actual = new List<string>().ToCsvString();
+
+            actual.Should().Be(expected);
+        }
+
+        [Fact]
+        public void ShouldWorkCorrectlyForOneItem()
+        {
+            var expected = "[test]";
+
+            var actual = new List<string> { "test" }.ToCsvString();
+
+            actual.Should().Be(expected);
+        }
+        
+        [Fact]
+        public void ShouldWorkCorrectlyForMultipleItems()
+        {
+            var expected = "[test,test2]";
+
+            var actual = new List<string> { "test", "test2" }.ToCsvString();
+
+            actual.Should().Be(expected);
+        }
+    }
+}
