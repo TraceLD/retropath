daff5d8d81e4d02129780e848391f2780872d3f8
TraceLD <lukaszdabrowski881@gmail.com>
2022-07-24 00:27:49 +0100

    fix an incredibly vile typo

--- src/RetroPath.Core/RuleEngine.cs
+++ src/RetroPath.Core/RuleEngine.cs
@@ -43,7 +43,7 @@ public class RuleEngine
 
                     foreach (var r in monoRes)
                     {
-                        monoRes.Add(r);
+                        results.Add(r);
                     }
                 }
                 else
